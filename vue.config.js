65dae2c23d9f1a5ebfba105ec0eaa4c39811a27b
leonzh2k <leonzh2k@gmail.com>
2021-03-01 20:12:30 -0800

    committed dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/DataViz-Dashboard/'
-      : '/'
-}
+  publicPath: "/StackOverflow2020Viz",
+  outputDir: "docs"
+};
